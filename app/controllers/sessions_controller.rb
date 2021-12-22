c434026d260a3221a3d973c899c99d7529534565
Landon Skinner <landonjskinner@gmail.com>
2021-12-22 08:20:33 -0600

    commit tree 3a7e4526e58cb73d72197554e22e7e766e29fa54

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unauthorized
+        end
+    end
+
+end
